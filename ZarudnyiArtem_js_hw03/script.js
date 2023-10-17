139d25206178ab6cc34741431add1a988d19a79d
Artem Zarudnyi <142827893+artez10@users.noreply.github.com>
2023-10-17 19:18:11 +0200

    Add files via upload

---
+++ ZarudnyiArtem_js_hw03/script.js
@@ -0,0 +1,5 @@
+let firstNumb = +prompt("write first number");
+let secondNumb = +prompt("write second number");
+let thirdNumb = +prompt("write third number");
+
+alert (`${(firstNumb + secondNumb + thirdNumb)/3}`);
